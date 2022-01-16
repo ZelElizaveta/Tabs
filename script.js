43a44068a0fc55ffa481638c5acf754ebb0cc210
Elizaveta Zelenova <lizastepanova93@mail.ru>
2022-01-16 20:28:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-window.addEventListener('DOMContentLoaded', function() { 
+window.addEventListener('DOMContentLoaded', function() {                //Назначаем обработчик событий на всю страницу. 
 
     'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
+    let tab = document.querySelectorAll('.info-header-tab'),              // Обьявляем переменные и  добираемся до интересующих нас элементов из HTML структуры, 
+        info = document.querySelector('.info-header'),                    // это сами Табы (info-header-tab), родительский блок для Табов (info-header) 
+        tabContent = document.querySelectorAll('.info-tabcontent');        // и содержание Табов (info-tabcontent)
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
+    function hideTabContent(a) {                                          // Прописываем функцию, которая будет скрывать все элементы tabContent
+        for (let i = a; i < tabContent.length; i++) {                     // (show и hide это классы, которые прописаны в CSS, мы можем увидеть их в style.css)
+            tabContent[i].classList.remove('show');                        // сначала удалям класс show   
+            tabContent[i].classList.add('hide');                        // затем добавляем класс hide
         }
     }
 
-    hideTabContent(1);
+    hideTabContent(1);                                             // Запускаем функцию, скрываются все Табы, кроме первого.
 
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
+    function showTabContent(b) {                                   // Эта функция показывает наш  tabContent 
+        if (tabContent[b].classList.contains('hide')) {             // запускаем цикл, который проверяет скрыт ли  tabContent
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function(event) {                    // назначаем обработчик событий на родителя tabContent 
         let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
+        if (target && target.classList.contains('info-header-tab')) {       //проверяем действительно ли мы нажали на tabContent, а не на его родителя info
+            for(let i = 0; i < tab.length; i++) {                           // и запускаем цикл, который будет сравнивать  target и номер tabContent                                                 if (target == tab[i]) {
+                    hideTabContent(0);                                    //скрываем все Табы
+                    showTabContent(i);                                   //показывает необходимый нам Таб           
                     break;
                 }
             }
